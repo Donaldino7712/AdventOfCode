fac7b07a9e33049125a6ae87d3bd1fca8d9293e4
Donaldino7712 <donaldino7712@gmail.com>
2023-12-01 21:38:00 +0100

    Day 1, 2023 solved!

---
+++ 2023/Day 1/Part 1.js
@@ -0,0 +1,10 @@
+import { input } from "./inputData.js"
+let result = 0
+let array = []
+input.forEach(e => {
+    let g = []
+    for (let i = 0; i < e.length; i++) if (/[0-9]/.test(e[i])) g.push(e[i])
+    array.push(Number(g[0] + (g.length == 1 ? g[0] : g[g.length - 1])))
+})
+array.forEach(i => result += i)
+console.log(result)
